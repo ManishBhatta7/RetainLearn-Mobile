c146edf0f308a70c46a7e0e9846dbc251df0c1c1
Manish Bhatta <manishgamez@gmail.com>
2025-12-20 16:03:59 +0530

    fix(flutter): correct import path for RetainLearnTheme in shell

--- lib/presentation/layouts/retain_learn_shell.dart
+++ lib/presentation/layouts/retain_learn_shell.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../core/theme/retain_learn_theme.dart';
+import '../../core/theme/retain_learn_theme.dart';
 
 /// Responsive Shell for RetainLearn
 ///
